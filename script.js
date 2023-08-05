e31013fa43881313b87971b082cbac1ae3e8543e
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-08-06 00:48:01 +0530

    .

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-let links = document.querySelectorAll('.list .link');
-let sections = document.querySelectorAll('section[id]');
-
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    let href = link.getAttribute('href');
-    document.querySelector(`${href}`).scrollIntoView({ behavior: 'smooth' });
-  });
-});
-
-window.addEventListener('scroll', () => {
-  let scrollY = window.pageYOffset;
-  sections.forEach((section) => {
-    const sectionHeight = section.offsetHeight;
-    const sectionTop = section.offsetTop - 200;
-    const sectionId = section.getAttribute('id');
-    if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
-      document
-        .querySelector(`.list a[href*="${sectionId}"]`)
-        .parentElement.classList.add('active');
-    } else {
-      document
-        .querySelector(`.list a[href*="${sectionId}"]`)
-        .parentElement.classList.remove('active');
-    }
-  });
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
